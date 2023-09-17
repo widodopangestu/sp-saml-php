1e49d481abd4f3658057814d127652aeee88269a
Widodo Pangestu <wid.pangestu@gmail.com>
2023-09-17 08:44:47 +0700

    Update info.php

--- demo1/info.php
+++ demo1/info.php
@@ -1,4 +1,12 @@
 <?php
 echo getenv('SP_BASE_URL');
+echo '<hr/>';
 
 echo getenv('IDP_X509CERT');
+echo '<hr/>';
+
+echo getenv('IDP_SSO_URL');
+echo '<hr/>';
+
+echo getenv('IDP_SLO_URL');
+echo '<hr/>';
